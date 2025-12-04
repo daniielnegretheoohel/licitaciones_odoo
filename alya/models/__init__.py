5c705b45455eeca65a46b6082cd5add16a4e964e
daniielnegretheoohel <geovani.oohel@gmail.com>
2025-12-04 14:04:02 -0600

    Delete alya directory

--- alya/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import alya_metas_personales
-from . import alya_etiquetas_metas
-from . import alya_recomendaciones_inteligentes
-from . import alya_seguimiento_actividades
-from . import res_partner
